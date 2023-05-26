ccd933452dbc224e4415b95230e45ae5d2cadef8
Refilwe93T <refilwe.p.tau@live.com>
2023-05-26 04:50:37 -0700

    2-largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,18 +13,17 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a > b && a > c)
-    {
+	{
 	largest = a;
-    }
+	}
 	else if (b > c && b > a)
-    {
+	{
 	largest = c;
-    }
+	}
 	else
-    {
+	{
 	largest = b;
-    }
+	}
 
 	return (largest);
 }
-
